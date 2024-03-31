78a88480935d94b3f9c008814270913a5aea6dbd
Your name <wedoulilishte@gmail.com>
2024-03-31 22:26:25 +0300

    Map exercises from an array

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ import styles from "./page.module.css";
 export default function Home() {
   const data = "Hard";
 
-  const muscleGroups = ["biceps", "lats", "forearms"]
+  const exampleValues = [
+    {
+      id: "1",
+      name: "Pull-up",
+      difficulty: "Hard",
+      muscleGroups: ["biceps", "lats", "forearms"]
+    }
+]
+
+  //const muscleGroups = ["biceps", "lats", "forearms"]
 
   const SaveExercise = (id: string): void => {
     // use local storage to save the exercise' id
@@ -15,7 +24,31 @@ export default function Home() {
   return (
     <>
     <div className={styles.exercisesContainer + " flex-column"}>
-      <div className={styles.exercise}>
+      {
+        exampleValues.map((item, index) => 
+        <div className={styles.exercise} key={index}>
+        <div className={styles.exerciseFirstRow + " flex"}>
+          <label className={styles.firstRowInfo}>{item.name}</label>
+
+          <label className={styles.firstRowInfo + " flex"}>Difficulty:  
+           <label style={{color: item.difficulty == "Hard" ? "red" : "green", marginLeft: "10px"}}>{item.difficulty}</label>
+           <img src="" alt="" className={styles.img} />
+          </label>
+          
+          <img src="" alt="" className={styles.img} onClick={() => SaveExercise("1")}/>
+        </div>
+
+        <div className={"flex"}>
+          <label style={{marginLeft: "10px", fontSize: "1.1rem"}}>Muscle groups:</label>
+          {
+            item.muscleGroups.map((innerItem, innerIndex) => <label className={styles.muscleGroup} key={innerIndex}>{innerItem}</label>)
+          }
+        </div>
+
+      </div>
+        )
+      }
+      {/* <div className={styles.exercise}>
         <div className={styles.exerciseFirstRow + " flex"}>
           <label className={styles.firstRowInfo}>Push-ups</label>
 
@@ -34,7 +67,7 @@ export default function Home() {
           }
         </div>
 
-      </div>
+      </div> */}
     </div>
     </>
   );
